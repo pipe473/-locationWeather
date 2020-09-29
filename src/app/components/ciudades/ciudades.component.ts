2b3c6206c3c60bd7871f68c112bd0e814a023558
pipe473 <bedoyafelipe743@gmail.com>
2020-09-29 15:13:56 +0200

    Clase de clima añadida correctamente, falta mostrar los datos del clima por pantalla

--- src/app/components/ciudades/ciudades.component.ts
+++ src/app/components/ciudades/ciudades.component.ts
@@ -5,32 +5,42 @@ import { Clima } from 'src/app/models/clima';
 @Component({
   selector: 'app-ciudades',
   templateUrl: './ciudades.component.html',
-  styleUrls: ['./ciudades.component.scss']
+  styleUrls: ['./ciudades.component.scss'],
 })
 export class CiudadesComponent implements OnInit {
+  public ciudades: Ciudad[];
 
-  public ciudades: Ciudad[]; 
-
-  constructor(){
+  constructor() {
     this.ciudades = [
-      new Ciudad ('Madrid', 'España', 'MAD', new Clima(20, 200, 60)),
-      new Ciudad ('Cali', 'Colombia', 'CL', new Clima(20, 200, 60)),
-      new Ciudad ('Florencia', 'Italia', 'FL', new Clima(20, 200, 60))
-    ]
+      new Ciudad('Madrid', 'España', 'MAD', new Clima(20, 200, 60)),
+      new Ciudad('Cali', 'Colombia', 'CL', new Clima(20, 200, 60)),
+      new Ciudad('Florencia', 'Italia', 'FL', new Clima(20, 200, 60)),
+    ];
   }
 
-// FUNCION QUE ME CREA UN  NUEVO CLIMA
-  dataWeight(ciudad: HTMLInputElement, pais: HTMLInputElement, codigo: HTMLInputElement, temperatura: HTMLInputElement, viento: HTMLInputElement, precipitaciones: HTMLInputElement ){
+  // FUNCION QUE ME CREA UN  NUEVO CLIMA
+  dataWeight(
+    ciudad: HTMLInputElement,
+    pais: HTMLInputElement,
+    codigo: HTMLInputElement,
+    temperatura: HTMLInputElement,
+    viento: HTMLInputElement,
+    precipitaciones: HTMLInputElement
+  ) {
     console.log(this.ciudades);
-        let ciudadNueva = new Ciudad(String(ciudad.value), String (pais.value), String (codigo.value), new Clima(Number (temperatura.value), Number(viento.value), Number(precipitaciones.value)))
-        this.ciudades.push(ciudadNueva);
-        return ciudadNueva;
+    let ciudadNueva = new Ciudad(
+      String(ciudad.value),
+      String(pais.value),
+      String(codigo.value),
+      new Clima(
+        Number(temperatura.value),
+        Number(viento.value),
+        Number(precipitaciones.value)
+      )
+    );
+    this.ciudades.push(ciudadNueva);
+    return ciudadNueva;
     // console.log(this.ciudades);
   }
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
-
-
-
