cb98efe32e42c6590a2d438b99c6873e43a09453
pipe473 <bedoyafelipe743@gmail.com>
2020-09-29 13:44:29 +0200

    Creando funcionalidad para crear nuevos datos, esta fallando porque no recoge los datos del input

--- src/app/components/ciudades/ciudades.component.ts
+++ src/app/components/ciudades/ciudades.component.ts
@@ -8,17 +8,28 @@ import { Ciudad } from 'src/app/models/ciudad';
 })
 export class CiudadesComponent implements OnInit {
 
-  ciudades: Ciudad[]; 
+  public ciudades: Ciudad[]; 
 
   constructor(){
     this.ciudades = [
       new Ciudad ('Madrid', 'España', 'MAD'),
       new Ciudad ('Cali', 'Colombia', 'CL'),
-      new Ciudad ('Barcelona', 'España', 'BCN'),
       new Ciudad ('Florencia', 'Italia', 'FL')
     ]
+  }
+
+
+  dataWeight(ciudad: HTMLInputElement, pais: HTMLInputElement, codigo: HTMLInputElement ){
+    console.log(this.ciudades);
+        let ciudadNueva = new Ciudad(String(ciudad.value), String (pais.value), String (codigo.value))
+        this.ciudades.push(ciudadNueva);
+        return ciudadNueva;
+    // console.log(this.ciudades);
   }
   ngOnInit(): void {
   }
 
 }
+
+
+
